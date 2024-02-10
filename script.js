1df4bc1c91fd76c2c2096a8cf050c6abe4c4c63d
Badal Kumar <112364403+beyond-sky33@users.noreply.github.com>
2024-02-11 01:33:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let inp = document.querySelector("input");
+let add = document.querySelector("#add");
+let remove = document.querySelector("#remove");
+let ul = document.querySelector("ul");
+
+let li;
+
+add.addEventListener("click",function(){
+    if(inp.value.trim() === ''){}
+    else{
+        li = document.createElement('li');
+        li.textContent = inp.value;
+        ul.appendChild(li);
+        inp.value = "";
+    }
+})
+
+remove.addEventListener("click",function(){
+    ul.removeChild(li);
+})
+
+
